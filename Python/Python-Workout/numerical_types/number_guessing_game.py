e351f24c06bd1fbc217f289ef26dd25ff0ae09fb
Ashish Jha <ajlti2019@gmail.com>
2025-01-29 18:44:15 +0530

    Updated python workout forlder for code practise

---
+++ Python/Python-Workout/numerical_types/number_guessing_game.py
@@ -0,0 +1,18 @@
+'''
+Number Guessing Game
+Write a function (guessing_game) that takes no arguments.
+When run, the function chooses a random integer between 0 and 100 (inclusive).
+Then ask the user to guess what number has been chosen.
+Each time the user enters a guess, the program indicates one of the following:
+Too high
+Too low
+Just right
+If the user guesses correctly, the program exits. Otherwise, the user is asked to try again.
+The program only exits after the user guesses correctly.
+'''
+
+fruits = ['apple', 'banana', 'cherry']
+colors = ['red', 'yellow', 'black']
+combined = zip(fruits, colors)
+for i in combined:
+    print(i)
