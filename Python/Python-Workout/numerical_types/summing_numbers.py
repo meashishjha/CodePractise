faee0602eb3213c1a5cfbc9e58ae2df76b113aff
Ashish Jha <ajlti2019@gmail.com>
2025-01-31 05:40:36 +0530

    commiting numbers problem

---
+++ Python/Python-Workout/numerical_types/summing_numbers.py
@@ -0,0 +1,10 @@
+'''
+The challenge here is to write a mysum function that does the same thing as the built-in sum function.
+However, instead of taking a single sequence as a parameter, it should take a variable number of arguments.
+Thus, although you might invoke sum([1,2,3]), you’d instead invoke mysum(1,2,3) or mysum(10,20,30,40,50).
+'''
+
+def mysum(*args):
+    pass
+
+
