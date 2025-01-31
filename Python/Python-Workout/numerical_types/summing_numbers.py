0a0644c8a0ee8fd7a9dc4deb52166083e3755bbc
Ashish Jha <ajlti2019@gmail.com>
2025-02-01 03:27:52 +0530

    summing number code completion

--- Python/Python-Workout/numerical_types/summing_numbers.py
+++ Python/Python-Workout/numerical_types/summing_numbers.py
@@ -4,7 +4,14 @@ However, instead of taking a single sequence as a parameter, it should take a va
 Thus, although you might invoke sum([1,2,3]), you’d instead invoke mysum(1,2,3) or mysum(10,20,30,40,50).
 '''
 
-def mysum(*args):
-    pass
+def mysum(*inputs):
+    total_sum = 0
+    for input in inputs:
+        total_sum += input
+    return total_sum
 
 
+
+if __name__ == "__main__":
+    print(mysum(2, 3, 7, 9, 11))
+    print(mysum())
