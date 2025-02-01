06b9d47df9ed18d16659a334d8401d8b103b1ec7
Ashish Jha <ajlti2019@gmail.com>
2025-02-01 09:42:34 +0530

    add function for pg_latin

---
+++ Python/Python-Workout/strings/pig_latin.py
@@ -0,0 +1,6 @@
+'''
+write a Python function (pig_latin) that takes a string as input, assumed to be an English word.
+The function should return the translation of this word into Pig Latin. You may assume that the
+word contains no capital letters or punctuation.
+'''
+
