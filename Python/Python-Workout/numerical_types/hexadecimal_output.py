f3bacf00462a341148f161457e1b9f94a60e57c0
Ashish Jha <ajlti2019@gmail.com>
2025-02-01 07:13:33 +0530

    function to add hexadecimal to decimal conversion

--- Python/Python-Workout/numerical_types/hexadecimal_output.py
+++ Python/Python-Workout/numerical_types/hexadecimal_output.py
@@ -6,9 +6,13 @@ although it’s permissible to use int one digit at a time.
 function to convert hexadecimal to  integer
 '''
 
-def hex_output():
-    pass
+def hex_output(number):
+    total_int = 0
+    for position, value in enumerate(reversed(number)):
+        total_int += int(value,16) * (16**position)
+
+    return total_int
 
 
 if __name__ == "__main__":
-    print(hex_output(50))
+    print(hex_output('50'))
