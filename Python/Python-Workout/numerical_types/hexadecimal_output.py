585a481ee72becf248f85d297d8b5681673a6dbb
Ashish Jha <ajlti2019@gmail.com>
2025-02-01 06:00:06 +0530

    python function to convert hexadecimal to integer

---
+++ Python/Python-Workout/numerical_types/hexadecimal_output.py
@@ -0,0 +1,14 @@
+'''
+you need to write a function (hex_output) that takes a hex number and returns the decimal equivalent.
+That is, if the user enters 50, you’ll assume that it’s a hex number (equal to 0x50) and will print
+the value 80 to the screen. And no, you shouldn’t convert the number all at once using the int function,
+although it’s permissible to use int one digit at a time.
+function to convert hexadecimal to  integer
+'''
+
+def hex_output():
+    pass
+
+
+if __name__ == "__main__":
+    print(hex_output(50))
