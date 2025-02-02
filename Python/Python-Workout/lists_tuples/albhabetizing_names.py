b89de6bb4f24f8866a8e415d1f15bc21b76ad5d9
Ashish Jha <ajlti2019@gmail.com>
2025-02-02 07:25:24 +0530

    adding alphabetizing names

---
+++ Python/Python-Workout/lists_tuples/albhabetizing_names.py
@@ -0,0 +1,17 @@
+'''
+PEOPLE = [{'first':'Reuven', 'last':'Lerner',
+    'email':'reuven@lerner.co.il'},
+ {'first':'Donald', 'last':'Trump',
+    'email':'president@whitehouse.gov'},
+ {'first':'Vladimir', 'last':'Putin',
+    'email':'president@kremvax.ru'}
+ ]
+write a function, alphabetize_names, that assumes the existence of a PEOPLE constant defined as shown in the code.
+The function should return the list of dicts, but sorted by last name and then by first name.
+'''
+
+def alphabetize_names():
+    pass
+
+if __name__ == "__main__":
+    print(alphabetize_names())
