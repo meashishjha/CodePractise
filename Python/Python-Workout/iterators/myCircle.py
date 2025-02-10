f6bd4ab55393dd23e16b5719720832df070c8076
Ashish Jha <ajlti2019@gmail.com>
2025-02-10 06:06:05 +0530

    Circle iterator

--- Python/Python-Workout/iterators/myCircle.py
+++ Python/Python-Workout/iterators/myCircle.py
@@ -4,3 +4,30 @@ The idea is that the object will then return elements the defined number of time
 If the number is greater than the number of elements, then the sequence repeats as necessary.
 You should define the class such that it uses a helper (which I call CircleIterator).
 '''
+
+class CircleIterator():
+    def __init__(self, data, length):
+        self.data = data
+        self.length = length
+        self.index = 0
+
+    def __next__(self):
+        if self.index >= self.length:
+            raise StopIteration
+        data_length = len(self.data)
+        value = self.data[self.index % data_length]
+        self.index += 1
+        return value
+
+class Circle():
+    def __init__(self, data, length):
+        self.data = data
+        self.length = length
+
+
+    def __iter__(self):
+        return CircleIterator(self.data, self.length)
+
+
+c = Circle('abc', 5)
+print(list(c))
